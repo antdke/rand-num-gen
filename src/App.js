e1c119a45613bab8a51da468e366bd767f7a27f8
Anthony Diké <anthonydike@nyu.edu>
2020-05-24 16:39:58 -0400

    feat: App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: 999 };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-body">
+          <div>
+            <p>{this.state.num}</p>
+          </div>
+          <form>
+            <label htmlFor="max">max: </label>
+            <input type="text" />
+            <br></br>
+            <label htmlFor="min">min: </label>
+            <input type="text" />
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
